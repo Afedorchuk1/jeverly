095aca3c0edc89841eab1b6b5c7d4244950208ed
nikita <mailboxpavlov@gmail.com>
2018-01-08 23:45:31 +0300

    add headr and small fix

--- js/cart.js
+++ js/cart.js
@@ -17,14 +17,14 @@ $.getJSON('goods.json', function (data) {
         else {
             var out = '';
             for (var key in cart) {
-                out += '<button class="delete" data-art="' + key + '" >x</button>';
-                out += '<img src="' + goods[key].image + '" width="48">';
-                out += goods[key].name;
-                out += '<button class="minus" data-art="' + key + '">-</button>';
-                out += cart[key];
-                out += '<button class="plus" data-art="' + key + '">+</button>';
-                out += cart[key] * goods[key].cost;
-                out += '<br>';
+                out += '<div class="row"><div class="cntr"><div class="col-xl-1 col-lg-1 col-sm-1"><button class="delete" data-art="' + key + '" >x</button></div>';
+                out += '<div class="col-xl-3 col-lg-3 col-sm-3"><img src="' + goods[key].image + '" width="100"></div>';
+                out += '<div class="col-xl-2 col-lg-2 col-sm-2">' + goods[key].name + '</div>';
+                out += '<div class="col-xl-1 col-lg-1 col-sm-1"><button class="minus" data-art="' + key + '">-</button></div>';
+                out += '<div class="col-xl-1 col-lg-1 col-sm-1">' + cart[key] + '</div>';
+                out += '<div class="col-xl-1 col-lg-1 col-sm-1"><button class="plus" data-art="' + key + '">+</button></div>';
+                out += '<div class="col-xl-3 col-lg-3 col-sm-3">' + cart[key] * goods[key].cost + '</div>';
+                out += '<br></div> </div>';
             }
             $('#my-cart').html(out);
             $('.plus').on('click', plusGoods);
